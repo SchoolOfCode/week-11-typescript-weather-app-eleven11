d3375fb2cc0b8ea671370664428b2d5fa8675dea
Sergio <sergeh96@gmail.com>
2025-02-19 14:30:29 +0000

    initial logic set up for handling state

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,38 @@
 import WeatherResult from "./Components/WeatherResult";
 import WeatherSearch from "./Components/WeatherSearch";
+import { useState } from "react";
 
 export default function Home() {
-  return (
-    <div>
-      <main className="flex-grow container mx-auto px-4">
-        <WeatherSearch className="py-11" />
-        <WeatherResult />
-      </main>
-    </div>
-  );
+  // Use state here to handle search input and fetched weather data
+  const [searchQuery, setSearchQuery] = useState("");
+  const [weatherData, setWeatherData] = useState(null);
+  // Lift state from the WeatherSearch up to the Home component
+  // Pass a function from home to search that updates search query state
+  const handleSearch = async (query: string) => {
+    try {
+      // Make the call to our defined backend route
+      const response = await fetch(`/api/request?query=${query}`);
+      // Parse the JSON response
+      const data = await response.json();
+      // Log the data to the console
+      console.log(data);
+      // Store fetched data in state
+      setWeatherData(data);
+    } catch (error) {
+      console.error(`Error fetching data:`, error);
+    }
+
+    return (
+      <div>
+        <main className="flex-grow container mx-auto px-4">
+          <WeatherSearch
+            className="py-11"
+            search={searchQuery}
+            setSearch={setSearchQuery}
+          />
+          <WeatherResult />
+        </main>
+      </div>
+    );
+  };
 }
--- src/pages/api/request.js
+++ src/pages/api/request.js
@@ -1,18 +1,25 @@
 import "dotenv/config";
 
 async function requestData(req, res) {
+  // Extract the 'query' parameter from the request query string
   let { query } = req.query;
 
   try {
+    // Make a request to the Weather API using the extracted query parameter
     const data = await fetch(
       `http://api.weatherapi.com/v1/forecast.json?key=${process.env.API_KEY}&q=${query}`
     );
 
+    // Parse the response as JSON
     let response = await data.json();
 
+    // Send the parsed response back to the client with a 200 status code
     res.status(200).json(response);
   } catch (error) {
+    // Log any errors to the console
     console.error(error);
+
+    // Send a 500 status code with an error message if something goes wrong
     res.status(500).json({ error: "Internal Server Error" });
   }
 }
